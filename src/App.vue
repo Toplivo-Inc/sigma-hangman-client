69d7aef942cb83e8bd37dccae091b6a6f6606fe9
slozhkai <sisipopakaka@gmail.com>
2026-01-08 01:45:41 +0300

    feat: add router viewer for app

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,8 @@ import Header from "@/components/Header.vue";
 <template>
   <v-app>
     <Header />
+    <v-main>
+      <router-view />
+    </v-main>
   </v-app>
 </template>
--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import App from "@/App.vue";
+import Auth from "@/pages/Auth.vue";
 
 const routes = [
-  { path: '/', component: App },
+  { path: '/auth', component: Auth },
+  {
+    path: '/',
+    component: App,
+    // children: [
+    //   { path: '', component: () => import('@/pages/Home.vue') },
+    // ]
+  },
 ]
 
 export default createRouter({
