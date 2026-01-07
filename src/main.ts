edd47c31f492be036ad8b3f851df70841c2ca98b
slozhkai <sisipopakaka@gmail.com>
2026-01-08 01:13:40 +0300

    feat: install vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles/main.css'
 import 'unfonts.css'
 import vuetify from './plugins/vuetify'
 import pinia from './plugins/pinia'
+import router from './plugins/router'
+
 import App from './App.vue'
-import '@mdi/font/css/materialdesignicons.css'
 
 createApp(App)
   .use(vuetify)
   .use(pinia)
+  .use(router)
   .mount('#app')
---
+++ src/plugins/router.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from "vue-router";
+import App from "@/App.vue";
+
+const routes = [
+  { path: '/', component: App },
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+})
