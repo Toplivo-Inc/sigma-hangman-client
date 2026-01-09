f0a495aa7fee82e2878f7d7111d5e68b7a03a159
slozhkai <sisipopakaka@gmail.com>
2026-01-09 03:11:05 +0300

    feat: add and styling auth page

---
+++ src/pages/Auth.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+
+import { ref } from "vue";
+import { validator } from "@/utils/validator.ts";
+
+const username = ref('')
+const password = ref('')
+const showPassword = ref(false)
+</script>
+
+<template>
+  <v-container
+    class="d-flex align-center justify-center"
+  >
+    <v-sheet width="600" elevation="4" rounded class="px-16 py-8">
+      <v-form fast-fail @submit.prevent>
+        <v-card-title>Войти</v-card-title>
+
+        <v-text-field
+          clearable
+          v-model="username"
+          :rules="validator.username"
+          label="Имя"
+          variant="solo"
+          required
+        />
+
+        <v-text-field
+          clearable
+          :type="showPassword ? 'text' : 'password'"
+          :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+          @click:append-inner="showPassword = !showPassword"
+          v-model="password"
+          :rules="validator.password"
+          label="Пароль"
+          variant="solo"
+          required
+        />
+
+        <v-btn
+          type="submit"
+          block
+          class="mt-4"
+          color="secondary"
+          text="Войти"
+        />
+      </v-form>
+    </v-sheet>
+  </v-container>
+</template>
--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import App from "@/App.vue";
 import Registration from "@/pages/Registration.vue";
+import Auth from "@/pages/Auth.vue";
 
 const routes = [
   { path: '/registration', component: Registration },
+  { path: '/auth', component: Auth },
   {
     path: '/',
     component: App,
